a39037b539f34c0b87982919f42babda56f70406
sandrorsjunior <hulk@DESKTOP-PLGLQAA>
2025-03-30 22:05:05 +0100

    conection with backend endpoint project

--- src/pages/Projects/ProjectCards.jsx
+++ src/pages/Projects/ProjectCards.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaLink } from "react-icons/fa"; // Link icon from react-icons
-import {data} from "./data";
 import { Link, Router, Routes } from "react-router-dom";
 
 // ProjectCard Component to display a single project
@@ -9,12 +8,12 @@ const ProjectCard = ({ logoSrc, title, description, link }) => {
     <div className="col-sm-12 col-md-6 col-lg-4">
       <div className="card mb-4 border border-0 rounded-5 card-projects p-2 shadow-sm">
         <div className="row justify-content-start">
-        <img
-          src={logoSrc}
-          alt={`${title} logo`}
-          className="card-img-top"
-          style={{ maxWidth: "70px", margin: "10px" }}
-        />
+          <img
+            src={logoSrc}
+            alt={`${title} logo`}
+            className="card-img-top"
+            style={{ maxWidth: "70px", margin: "10px" }}
+          />
         </div>
         <div className="row card-body">
           <h5 className="card-title">{title}</h5>
@@ -38,19 +37,34 @@ const ProjectCard = ({ logoSrc, title, description, link }) => {
 
 // MainProjects Component to display multiple projects
 export const ProjectCards = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      let response = await fetch("http://localhost:8080/project");
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      let result = await response.json();
+      setData(result);
+    };
+    getData();
+  }, []);
 
   return (
     <div className="">
       <div className="row">
-        {data.map((project, index) => (
-        <ProjectCard
-        key={index}
-        logoSrc={project.logoSrc}
-        title={project.title}
-        description={project.description}
-        link={project.link}
-      />
-        ))}
+        {data.map((project, index) => {
+          return (
+            <ProjectCard
+              key={index}
+              logoSrc={project.logoSrc}
+              title={project.title}
+              description={project.description}
+              link={project.link}
+            />
+          );
+        })}
       </div>
     </div>
   );
